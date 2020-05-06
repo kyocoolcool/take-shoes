0835664f163ddbd0ce8fddad2cbd339ecbd939d8
ChrisChen陳金昌 <kyocoolcool@hotmail.com>
2020-05-06 15:20:30 +0800

    modify

--- take-shoes-backend/src/main/java/kyocoolcool/takeshoes/config/DruidMonitorConfig.java
+++ take-shoes-backend/src/main/java/kyocoolcool/takeshoes/config/DruidMonitorConfig.java
@@ -30,7 +30,7 @@ public class DruidMonitorConfig {
         ServletRegistrationBean<Servlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(),
                 "/druid/*");
         // IP白名单
-        servletRegistrationBean.addInitParameter("allow", "192.168.1.101,127.0.0.1");
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
         // IP黑名单(共同存在时，deny优先于allow)
         servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
         // 控制台管理用户

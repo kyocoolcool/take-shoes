0fb974350ff67a8ea5739347fcbc0cab42cdcfef
ChrisChen陳金昌 <kyocoolcool@hotmail.com>
2020-07-24 15:52:44 +0800

    security config

--- take-shoes-backend/src/main/java/kyocoolcool/takeshoes/config/SecurityConfig.java
+++ take-shoes-backend/src/main/java/kyocoolcool/takeshoes/config/SecurityConfig.java
@@ -1,18 +1,13 @@
 package kyocoolcool.takeshoes.config;
 
-import kyocoolcool.takeshoes.entity.Reader;
-import kyocoolcool.takeshoes.repository.ReaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
@@ -81,4 +76,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //            }
 //        });
 //    }
+
 }
